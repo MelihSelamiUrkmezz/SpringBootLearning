18c9f108ebb5f291ab94f7a884a59621c05ecc8a
MelihSelamiUrkmez <melih.s.urkmez@gmail.com>
2023-01-16 10:40:30 +0300

    Add spring project based

---
+++ questapp/src/test/java/com/melihselamiurkmez/questapp/QuestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melihselamiurkmez.questapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
